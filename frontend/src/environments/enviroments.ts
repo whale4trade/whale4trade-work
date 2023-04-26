f7b8a7ddf12038f36c9b84df8544421fb521f89b
Your Name <louyhany999@gmail.com>
2023-04-26 22:04:34 +0200

    d

--- frontend/src/environments/enviroments.ts
+++ frontend/src/environments/enviroments.ts
@@ -1,5 +1,5 @@
 const env = {
-  url: "http://localhost:5000/api",
-  ver: "http://localhost:5000",
+  url: "https://whale-msfb.onrender.com/api",
+  ver: "https://whale-msfb.onrender.com",
 };
 export default env;
