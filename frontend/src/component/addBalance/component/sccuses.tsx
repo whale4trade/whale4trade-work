1325b122d0d882d53eee8d030d65f6bbc8e6e3e2
Your Name <louyhany999@gmail.com>
2023-05-28 01:01:48 +0300

    baldd

--- frontend/src/component/addBalance/component/sccuses.tsx
+++ frontend/src/component/addBalance/component/sccuses.tsx
@@ -6,37 +6,41 @@ import env from "../../../environments/enviroments";
 
 const Success = () => {
   const [price, setPrice] = useState("");
-  useEffect(() => {
-    const priceURl = window.location.search.slice(41, -804);
-    const bla = Number(price) / Number(priceURl);
+  const priceURl = window.location.search.slice(41, -804);
+  const bla = Number(price) / Number(priceURl);
+  const getDol = async () => {
     try {
-      axios.get(`${env.url}/dol`).then((res) =>
-        setPrice(res.data.data.dollar).then(() => {
-          try {
-            axios
-              .get(`${env.url}/users/${JSON.parse(localStorage.user).id}`)
-              .then((res) => {
-                try {
-                  axios
-                    .patch(`${env.url}/users/balance/${res.data.data.id}`, {
-                      id: res.data.data.id,
-                      balance: Number(res.data.data.balance) + Number(bla),
-                    })
-                    .then((res) => {
-                      console.log(res);
-                    });
-                } catch (error) {
-                  console.log(error);
-                }
-              });
-          } catch (error) {}
-        })
-      );
+      await axios
+        .get(`${env.url}/dol`)
+        .then((res) => setPrice(res.data.data.dollar))
+        .then(() => {
+          updateBal();
+        });
     } catch (error) {
       console.log(error);
     }
+  };
+  const updateBal = async () => {
+    try {
+      await axios
+        .get(`${env.url}/users/${JSON.parse(localStorage.user).id}`)
+        .then((res) => {
+          try {
+            axios.patch(`${env.url}/users/balance/${res.data.data.id}`, {
+              id: res.data.data.id,
+              balance: Number(res.data.data.balance) + Number(bla),
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getDol();
   }, []);
-
   return (
     <>
       <FontAwesomeIcon className="icon-suc" icon={faThumbsUp} />
