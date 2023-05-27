1d5f424c3b875981e0f27e0833456816d1e0ef4b
Your Name <louyhany999@gmail.com>
2023-05-28 00:49:20 +0300

    dd

--- frontend/src/component/addBalance/component/sccuses.tsx
+++ frontend/src/component/addBalance/component/sccuses.tsx
@@ -1,8 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import env from "../../../environments/enviroments";
+
 const Success = () => {
-  const price = window.location.search.slice(41, -804);
-  console.log(price);
+  const [price, setPrice] = useState("");
+  useEffect(() => {
+    const priceURl = window.location.search.slice(41, -804);
+    const bla = Number(price) / Number(priceURl);
+    try {
+      axios.get(`${env.url}/dol`).then((res) => setPrice(res.data.data.dollar));
+    } catch (error) {}
+    try {
+      axios
+        .get(`${env.url}/users/${JSON.parse(localStorage.user).id}`)
+        .then((res) => {
+          try {
+            axios
+              .patch(`${env.url}/users/balance/${res.data.data.id}`, {
+                id: res.data.data.id,
+                balance: Number(res.data.data.balance) + Number(bla),
+              })
+              .then((res) => {});
+          } catch (error) {
+            console.log(error);
+          }
+        });
+    } catch (error) {}
+  }, []);
 
   return (
     <>
