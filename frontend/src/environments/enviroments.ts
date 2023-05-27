9ca23618f4d712231279399bc6339510484d7b05
Your Name <louyhany999@gmail.com>
2023-05-28 02:35:11 +0300

    css

--- frontend/src/environments/enviroments.ts
+++ frontend/src/environments/enviroments.ts
@@ -1,9 +1,9 @@
 const env = {
-  // aff: "http://localhost:3000",
-  // url: "http://localhost:5500/api",
-  // ver: "http://localhost:5500",
-  aff: "https://www.whale4trade.com",
-  url: "https://whale-6c0p.onrender.com/api",
-  ver: "https://whale-6c0p.onrender.com",
+  aff: "http://localhost:3000",
+  url: "http://localhost:5000/api",
+  ver: "http://localhost:5000",
+  // aff: "https://www.whale4trade.com",
+  // url: "https://whale-6c0p.onrender.com/api",
+  // ver: "https://whale-6c0p.onrender.com",
 };
 export default env;
