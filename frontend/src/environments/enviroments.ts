7bec6419d37794b9fcaff9a8367823e9cf7c8e56
Your Name <louyhany999@gmail.com>
2023-06-21 01:20:16 +0300

    asfga

--- frontend/src/environments/enviroments.ts
+++ frontend/src/environments/enviroments.ts
@@ -1,10 +1,10 @@
 const env = {
-  aff: "http://localhost:3000",
-  url: "http://localhost:8800/api",
-  ver: "http://localhost:8800",
-  // aff: "https://www.whale4trade.com",
-  // url: "https://whale-6c0p.onrender.com/api",
-  // ver: "https://whale-6c0p.onrender.com",
+  // aff: "http://localhost:3000",
+  // url: "http://localhost:8800/api",
+  // ver: "http://localhost:8800",
+  aff: "https://www.whale4trade.com",
+  url: "https://whale-6c0p.onrender.com/api",
+  ver: "https://whale-6c0p.onrender.com",
   // api_key: "078e15c223-1576b07eea-rui0so",
   api_key: "f367d4bdc1-2604a28b17-rw5kvq",
 };
