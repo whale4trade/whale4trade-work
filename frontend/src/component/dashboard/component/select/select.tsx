4361850fac80a652da77d8ba904e81dc27800bc8
louy999 <louyhany999@gmail.com>
2023-07-07 01:13:43 +0300

    asdhdfg

--- frontend/src/component/dashboard/component/select/select.tsx
+++ frontend/src/component/dashboard/component/select/select.tsx
@@ -9,31 +9,48 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 const Select = () => {
+  const reload = () => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 200);
+  };
   return (
     <>
       <NavLink className="select-user" to="">
-        <FontAwesomeIcon className="select-user " icon={faUsers} />
+        <FontAwesomeIcon
+          onClick={reload}
+          className="select-user "
+          icon={faUsers}
+        />
         <span className="title-s">Users</span>
       </NavLink>
       <NavLink className="select-bundle" to="bundle">
-        <FontAwesomeIcon className="select-bundle" icon={faB} />
+        <FontAwesomeIcon
+          onClick={reload}
+          className="select-bundle"
+          icon={faB}
+        />
         <span className="title-s">Bundle</span>
       </NavLink>
       <NavLink className="select-server" to="req">
-        <FontAwesomeIcon icon={faR} />
+        <FontAwesomeIcon onClick={reload} icon={faR} />
         <span className="title-s">req</span>
       </NavLink>
       <NavLink className="select-server" to="phones">
-        <FontAwesomeIcon icon={faPhone} />
+        <FontAwesomeIcon onClick={reload} icon={faPhone} />
         <span className="title-s">phones</span>
       </NavLink>
       <NavLink className="select-server" to="dollar">
-        <FontAwesomeIcon className="select-server" icon={faDollarSign} />
+        <FontAwesomeIcon
+          onClick={reload}
+          className="select-server"
+          icon={faDollarSign}
+        />
         <span className="title-s">dollar</span>
       </NavLink>
 
       <NavLink className="select-server" to="api">
-        <FontAwesomeIcon icon={faA} />
+        <FontAwesomeIcon onClick={reload} icon={faA} />
         <span className="title-s">api</span>
       </NavLink>
     </>
